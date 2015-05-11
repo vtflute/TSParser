7e507bcace53a0af4da24aa1b4a26946e2e30fba
vtflute <vtflute@gmail.com>
2015-05-12 01:12:52 +0800

    Fulfill TS Stream PES parsing procedure

--- TSStruct.py
+++ TSStruct.py
@@ -3,9 +3,6 @@ __author__ = 'xiao'
 import io
 import struct
 import sys
-import tkinter
-import tkinter.messagebox
-import tkinter.filedialog
 from ctypes import *
 
 class TSHeader(Union):
@@ -21,13 +18,13 @@ class TSHeader(Union):
 
         _anonymous_ = ("bits",)
         _fields_ =   [("bits", _TSHeader),
-                            ("value", c_uint),]
+                            ("int", c_uint),]
 
         def __init__(self, data):
                 super(TSHeader, self).__init__()
                 self.position = data.tell()
                 self.raw = data.read(4)
-                self.value = struct.unpack('I', self.raw)[0]
+                self.int = struct.unpack('I', self.raw)[0]
 
 class TSAdaptationField(Union):
         class _AdaptationField(BigEndianStructure):
@@ -53,31 +50,171 @@ class TSAdaptationField(Union):
                 _anonymous_ = ("bits",)
                 _fields_ = [
                     ("bits", _PCR),
-                    ("value", c_uint64),
+                    ("int", c_uint64),
                 ]
 
                 def __init__(self, data):
                         super(TSAdaptationField.PCR, self).__init__()
                         self.position = data.tell()
                         self.raw = data.read(6)
-                        self.value = int.from_bytes(self.raw, byteorder='big')
+                        self.int = int.from_bytes(self.raw, byteorder='big')
 
         _anonymous_ = ("bits", )
         _fields_ = [
             ("bits", _AdaptationField),
-            ("value", c_uint16)
+            ("int", c_uint16)
         ]
 
-        def	__init__(self, data):
+        def __init__(self, data):
                     super(TSAdaptationField, self).__init__()
                     self.position = data.tell()
                     self.raw = data.read(2)
-                    self.value = struct.unpack("H", self.raw)[0]
+                    self.int = struct.unpack("H", self.raw)[0]
                     self.optional_fields = io.BytesIO(data.read(self.adaptation_field_length - 1))
                     if  (self.PCR_flag == 1):
                         self.pcr = self.PCR(self.optional_fields)
                     if (self.OPCR_flag == 1):
                         self.opcr = self.PCR(self.optional_fields)
+
+
+class PTSPattern(Union):
+        class _Pattern (Structure):
+                _fields_ = [
+                    ('prefix', c_int64, 4),
+                    ('pts1', c_int64, 3),
+                    ('marker', c_int64, 1),
+                    ('pts2', c_int64, 15),
+                    ('marker', c_int64, 1),
+                    ('pts3', c_int64, 15),
+                    ('marker', c_int64, 1),
+                ]
+        _anonymous_ = ('bits',)
+        _fields_ = [
+            ('bits', _Pattern),
+            ('int', c_int64),
+        ]
+
+        def __int__(self, data):
+                super(PTSPattern, self).__init__()
+                self.position = data.tell()
+                self.raw = data.read(5)
+                self.int = int.from_bytes(self.raw, byteorder='big')
+                self.value = self.pts3 + self.pts2 << 15 + self.pts1 << 30
+
+class PES(Union):
+        class _PES(Structure):
+                _fields_ = [
+                    ('packet_start_code_prefix', c_int32, 24),
+                    ('stream_id', c_int32, 8),
+                    ('pes_packet_length', c_int16, 16),
+                ]
+
+        _anonymous_ = ("bits",)
+        _fields_ = [
+            ("bits", _PES),
+            ("int", c_int64)
+        ]
+
+        def isPaddingStream(self):
+                if self.stread_id == 0xBE:
+                        return True
+                else:
+                        return False
+
+        def isAuxillaryStream(self):
+                if self.stread_id == 0xBC or    \
+                    self.stream_id == 0xBF or   \
+                    self.stream_id == 0xF0 or   \
+                    self.stream_id == 0xF1 or   \
+                    self.stream_id == 0xFF or   \
+                    self.stream_id == 0xF2 or   \
+                    self.stream_id == 0xF8:
+                        return True
+                else:
+                        return False
+        def isMainStream(self):
+                if not (self.isPaddingStream() and self.isAuxillaryStream()):
+                        return  True
+                else:
+                        return False
+
+        class MainStream(Union):
+                class _Header(Structure):
+                        _fields_ = [
+                            ('prefix', c_uint16, 2),
+                            ('pes_scrambling_control', c_int16, 2),
+                            ('pes_priority', c_int16, 1),
+                            ('data_alignment_indicator', c_int16, 1),
+                            ('copyright', c_int16, 1),
+                            ('original_or_copy', c_int16, 1),
+                            ('pts_dts_flag', c_int16, 2),
+                            ('escr_flag', c_int16, 1),
+                            ('es_rate_flag', c_int16, 1),
+                            ('esm_trick_mode_flag', c_int16, 1),
+                            ('additional_copy_info_flag', c_int16, 1),
+                            ('pes_crc_flag', c_int16, 1),
+                            ('pes_extension_flag', c_int16, 1),
+                            ('pes_header_data_length', c_int8, 8),
+                        ]
+                _anonymous_ = ('bits', )
+                _fields_ = [
+                    ('bits', _Header),
+                    ('int', c_int32),
+                ]
+
+                def parsePTS(self, data):
+                        if self.pts_dts_flag == 0x2:
+                                self.PTS = PTSPattern(data)
+                        elif self.pts_dts_flag == 0x3:
+                                self.PTS = PTSPattern(data)
+                                self.DTS = PTSPattern(data)
+                        elif self.pts_dts_flag == 0x1:
+                                print('pts_dts_flag 0x1')
+                        elif self.pts_dts_flag == 0x0:
+                                print('pts_dts_flag 0x0')
+
+                def __init__(self, data):
+                        super(PES.MainStreamHeader, self).__init__();
+                        self.position = data.tell()
+                        self.raw = data.read(3)
+                        self.int = int.from_bytes(self.raw, byteorder='big')
+                        self.optional_fields = io.BytesIO(data.read(self.pes_header_data_length))       #including stuffing fields
+                        self.parsePTS(self.optional_fields)
+
+                        self.payload_length = self.pes_packet_length - self.pes_header_data_length
+                        self.payload = io.BytesIO(data.read(self.payload_type))
+
+
+
+        def __init__(self, data, payload_unit_start_indicator):
+                super(PES, self).__init__()
+                self.position = data.tell()
+                self.raw = data.read(6)
+                self.int = int.from_bytes(self.raw, byteorder='big')
+                #TODO not handle pes_packet_length == 0
+                self.variable_fields = io.BytesIO(data.read(self.pes_packet_length))
+
+                if self.isMainStream():
+                        self.type = 'MainStream'
+                        self.stream = self.MainStream(data)
+                elif self.isAuxillaryStream():
+                        self.type = 'AuxillaryStream'
+                elif self.isPaddingStream():
+                        self.type = 'PaddingStream'
+
+class TSPayloadFactory(object):
+
+        def __init__(self, data, payload_unit_start_indicator):
+                self.probe(data)
+
+        def probe(self,data):
+                prefix = int.from_bytes(data.read(4), byteorder='big')
+                if prefix & 0x00000100:
+                        return 'PES'
+                else:
+                        return "PSI"
+                data.seek(-4, whence=io.SEEK_CUR)
+
 
 
 class TSPacket(object):
@@ -90,8 +227,12 @@ class TSPacket(object):
                         self.adaption_field = TSAdaptationField(filehandle)
             if self.head.adaptation_field_ctrl & 0x1:
                         #Parse payload field data
-                        self.payload_field
+                        self.payload = TSPayloadFactory(filehandle, self.head.payload_unit_start_indicator)
 
+##Following for Test
+import tkinter
+import tkinter.messagebox
+import tkinter.filedialog
 
 def getFilename():
     root=tkinter.Tk()
